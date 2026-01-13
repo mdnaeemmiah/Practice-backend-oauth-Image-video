2dfe1fc24dfe19ed8104b1948fbb9f18c4968a52
mdnaeemmiah <mdnaeemmiah48@gmail.com>
2026-01-14 02:39:11 +0600

    added

--- src/middlewares/NotFound.ts
+++ src/middlewares/NotFound.ts
@@ -15,10 +15,10 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = 500;
   let message = 'Something went wrong! ';
   let errorSources: TErrorSources = [
-    // {
-    //   path: '',
-    //   message: 'Something went wrong  ',
-    // },
+    {
+      path: '',
+      message: 'Something went wrong  ',
+    },
   ];
 
   if (err instanceof ZodError) {
