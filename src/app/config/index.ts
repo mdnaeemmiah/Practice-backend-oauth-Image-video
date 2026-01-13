5d3de3d65085f8cca3315b845779e929c64882ef
mdnaeemmiah <mdnaeemmiah48@gmail.com>
2026-01-14 01:07:51 +0600

    added

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -16,10 +16,7 @@ const emailPort = parseInt(
 
 export default {
   port: process.env.PORT || '5000',
-  database_url:
-    process.env.DATABASE_URL ||
-    process.env.DB_URL ||
-    'mongodb://localhost:27017/practice_db',
+  database_url:process.env.DB_URL,
   bcrypt_salt_rounds: process.env.Bcrypt_Salt_Round,
   NODE_ENV: process.env.NODE_ENV,
 
